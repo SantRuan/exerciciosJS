c699833adb901fc1a0adf2050b2049468092f874
Ruan Santiago <ruanvansantiago@gmail.com>
2021-11-25 17:57:16 -0300

    delete e edit adicionados

--- APILivros/app.js
+++ APILivros/app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 const port = 3000 // Inicializando a porta
 app.use(cors())
-app.use(bodyParser.urlencoded({extended: false})) 
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 
@@ -12,7 +12,7 @@ let books = []
 
 // Criando um endpoint get, para quando o usuario acessar, uma informação irá aparecer na tela
 
-app.post('/book', (req,res)=>{
+app.post('/book', (req, res) => {
     const book = req.body
     console.log(book)
     res.send('O livro foi adicionado ao banco de dados')
@@ -23,11 +23,11 @@ app.get('/books', (req, res) => {
     res.json(books);
 });
 
-app.get('/book/:isbn',(res,req)=>{
+app.get('/book/:isbn', (res, req) => {
     const isbn = req.param.isbn
 
-    for(let book in books){
-        if (book.isbn === isbn){
+    for (let book in books) {
+        if (book.isbn === isbn) {
             res.json(book)
             return;
         }
@@ -36,28 +36,41 @@ app.get('/book/:isbn',(res,req)=>{
     res.status(404).send('Book not Found')
 })
 
-app.delete('/book/:isbn', (req,res)=>{
+app.delete('/book/:isbn', (req, res) => {
 
-        const isbn = req.params.isbn
-        const newBook = req.body
-
+    const isbn = req.params.isbn
+    // Removendo o item do arry de livros
+    book = books.filter(i => {
+        if (i.isbn !== isbn) {
+            return true;
+        }
+        return false
+    })
+    res.send('O livro foi deletado')
 })
 
 
-app.get('/', (req,res)=>{
-    res.send('Ola mundo')
+app.post('/book/:isbn', (req, res) => {
+    const isbn = req.params.isbn
+    const newBook = req.body
+
+
+    // Removendo o item do array
+
+    for (let i = 0; i < books.length; i++) {
+
+        let book = books[i]
+
+        if (book.isbn === newBook) {
+            books[i] = newBook
+        }
+    }
+
+    res.send('O livro foi editado')
 })
 
 
 
 
 
-
-// Criando a API
-
-
-
-
-
-
-app.listen(port,()=> console.log(`Voce acessou a porta ${port}`))
+app.listen(port, () => console.log(`Voce acessou a porta ${port}`))
